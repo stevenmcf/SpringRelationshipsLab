aed42d516634c40e0fd0ea9789f19d89a21cf7d9
stmcf <stevenmcfarlane@hotmail.co.uk>
2020-12-01 10:28:03 +0000

    initial commit

---
+++ src/main/java/com/codeclan/example/SpringRelationshipsLab/SpringRelationshipsLabApplication.java
@@ -0,0 +1,13 @@
+package com.codeclan.example.SpringRelationshipsLab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRelationshipsLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRelationshipsLabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codeclan/example/SpringRelationshipsLab/SpringRelationshipsLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeclan.example.SpringRelationshipsLab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRelationshipsLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
